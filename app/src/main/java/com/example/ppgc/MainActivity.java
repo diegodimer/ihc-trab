a9b2c911dfe894b53409e7123485add1b114a2c1
rcastilhos <rodrigo_cteixeira@hotmail.com>
2021-10-14 16:44:48 -0300

    alteracoes na verificacao de senhas

--- app/src/main/java/com/example/ppgc/MainActivity.java
+++ app/src/main/java/com/example/ppgc/MainActivity.java
@@ -5,8 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int[] matriculas = {285642, 287691, 303030};
+    private int[] senhas = {285642, 287691, 303030};
+
+    TextView id;
+    TextView password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +23,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean verificalogin() {
+        id = findViewById(R.id.id);
+        password = findViewById(R.id.password);
+
         //TODO
-        //retorna true se id e senha baterem, senão false
-        return true;
+        //retorna true se id e senha estao certos, senão false
+        for(int i = 0; i < matriculas.length; i++) {
+            if (matriculas[i] == Integer.parseInt(id.getText().toString())){
+                for(int j = 0; j < senhas.length; j++){
+                    if(senhas[j] == Integer.parseInt(password.getText().toString()))
+                        return true;
+                }
+            }
+        }
+        return false;
     }
 
     public void Send(View view) {
@@ -26,8 +45,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
         }
         else{
-            //TODO
-            //jogar mensagem na tela dizendo que deu ruim
+            Toast.makeText(getApplicationContext(), "Matricula ou senha inválidas", Toast.LENGTH_LONG).show();
         }
 
     }
