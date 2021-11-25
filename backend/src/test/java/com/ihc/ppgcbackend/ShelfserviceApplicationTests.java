69643093b836a29fca464248eefe5317d0100de6
Diego Dimer <diego.dimer@ufrgs.br>
2021-11-24 22:58:31 -0300

    add advisor

--- backend/src/test/java/com/ihc/ppgcbackend/ShelfserviceApplicationTests.java
+++ backend/src/test/java/com/ihc/ppgcbackend/ShelfserviceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShelfserviceApplicationTests {
+class PPGCBackend {
 
 	@Test
 	void contextLoads() {
